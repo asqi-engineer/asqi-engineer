d8cac30b0eb07f80369b37ecadf931b4ea0577ad
Mayank Soni <mayank.soni101@gmail.com>
2025-12-19 11:13:20 +0800

    Add dataset loading functions

---
+++ src/asqi/datasets.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset, Dataset
+
+
+def load_hf_dataset(loader_params: dict[str, str]) -> Dataset:
+    """Load a HuggingFace dataset using the provided loader parameters.
+
+    Args:
+        loader_params (dict[str, str]): Keyword arguments for datasets.load_dataset function.
+
+    Returns:
+        Dataset: Loaded HuggingFace dataset.
+    """
+    dataset = load_dataset(**loader_params)
+    return dataset
